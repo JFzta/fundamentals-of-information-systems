a7eee0fcdced89fbaf520d313fcf31d03979fe39
JFzta <147529086+JFzta@users.noreply.github.com>
2025-04-07 14:07:55 -0500

    Initial version of proyecto2025 generated by generator-jhipster@8.10.0

---
+++ src/main/java/com/udea/lab/aop/logging/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Logging aspect.
+ */
+package com.udea.lab.aop.logging;
---
+++ src/main/java/com/udea/lab/service/InvalidPasswordException.java
@@ -0,0 +1,10 @@
+package com.udea.lab.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+}
